e16541bbed61396b9c49631e0b97e41c5bcd272c
sharkioso <tetkin04@mail.ru>
2025-07-20 18:26:26 +0500

    аспекты внедрены в работу

--- synthetic-starter/src/main/java/com/waylend/synthetic_starter/command/srevice/CommandService.java
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/command/srevice/CommandService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.waylend.synthetic_starter.command.model.CommandRequest;
 import com.waylend.synthetic_starter.command.model.Prioryty;
+import com.waylend.synthetic_starter.monitoring.Audit.WeylandWatchingYou;
+import com.waylend.synthetic_starter.monitoring.Audit.sender.AuditMode;
+import com.waylend.synthetic_starter.monitoring.metrics.WithMetrics;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,15 +18,14 @@ import lombok.extern.slf4j.Slf4j;
 public class CommandService {
     private final ExecutorService executor = Executors.newFixedThreadPool(4);
 
+    @WeylandWatchingYou(mode = AuditMode.CONSOLE)
+    @WithMetrics
     public void executeCommand(CommandRequest command, Runnable task) {
         if (command.getPrioryty() == Prioryty.CRITICAL) {
             log.info("Выполняется CRITICAL команда :{}", command.getDescription());
             task.run();
         } else {
-            executor.submit(() -> {
-                log.info("Обрабатывается COMMON команду:{}", command.getDescription());
-                task.run();
-            });
+            executor.submit(() ->task.run());
         }
     }
 
