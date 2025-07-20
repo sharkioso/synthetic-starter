2fb641a69818786d401a2d303498391ad1ca19a0
sharkioso <tetkin04@mail.ru>
2025-07-20 17:30:26 +0500

    сделал кафка аудит

---
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/monitoring/sender/KafkaAuditSender.java
@@ -0,0 +1,23 @@
+package com.waylend.synthetic_starter.monitoring.sender;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@ConditionalOnClass(KafkaTemplate.class)
+public class KafkaAuditSender implements AuditSender{
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    @Override
+    public void send(AuditMode mode, String methodName, Object[] args, Object result) {
+        String message = String.format("Method: %s | Args: %s | Result: %s",methodName,args,result);
+        kafkaTemplate.send("weyland-audit-topic",message);
+    }
+
+}
