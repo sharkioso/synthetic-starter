9aaf8bbadf0587e32a6ffdb711e9e799c217cd69
sharkioso <tetkin04@mail.ru>
2025-07-21 01:54:30 +0500

    ...

--- synthetic-starter/src/main/java/com/waylend/synthetic_starter/autoconfig/SyntheticAutoConfiguration.java
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/autoconfig/SyntheticAutoConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import com.waylend.synthetic_starter.command.srevice.CommandService;
 import com.waylend.synthetic_starter.monitoring.Audit.AuditAspect;
 import com.waylend.synthetic_starter.monitoring.Audit.sender.AuditSender;
+import com.waylend.synthetic_starter.monitoring.Audit.sender.ConsoleAuditSender;
 import com.waylend.synthetic_starter.monitoring.Audit.sender.KafkaAuditSender;
 import com.waylend.synthetic_starter.monitoring.metrics.MetricsAspect;
 
@@ -21,33 +23,30 @@ import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 public class SyntheticAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean
     public CommandService commandService() {
         return new CommandService();
     }
 
     @Bean
-    @ConditionalOnMissingBean
     public AuditAspect auditAspect(AuditSender sender) {
         return new AuditAspect(sender);
     }
 
     @Bean
-    @ConditionalOnMissingBean
     public MeterRegistry meterRegistry()
     {
         return new SimpleMeterRegistry();
     }
     
     @Bean
-    @ConditionalOnClass(name = "org.springframework.kafka.core.KafkaTemplate")
+    @ConditionalOnClass(KafkaTemplate.class)
     @ConditionalOnProperty(name = "weyland.audit.mode", havingValue = "KAFKA")
     public AuditSender kafkaAuditSender(KafkaTemplate<String, String> kafkaTemplate) {
         return new KafkaAuditSender(kafkaTemplate);
     }
 
+
     @Bean
-    @ConditionalOnProperty(name = "weyland.audit.mode", havingValue = "CONSOLE", matchIfMissing = true)
     public MetricsAspect metricsAspect(MeterRegistry registry) {
         return new MetricsAspect(registry);
     }
