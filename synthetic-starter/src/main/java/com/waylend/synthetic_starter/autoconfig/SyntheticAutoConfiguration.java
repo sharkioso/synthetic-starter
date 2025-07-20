e604e1202fd6c063cb36e1b31a76772b8d11d172
sharkioso <tetkin04@mail.ru>
2025-07-21 02:17:36 +0500

    v4

--- synthetic-starter/src/main/java/com/waylend/synthetic_starter/autoconfig/SyntheticAutoConfiguration.java
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/autoconfig/SyntheticAutoConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import com.waylend.synthetic_starter.command.srevice.CommandService;
 import com.waylend.synthetic_starter.monitoring.Audit.AuditAspect;
 import com.waylend.synthetic_starter.monitoring.Audit.sender.AuditSender;
+import com.waylend.synthetic_starter.monitoring.Audit.sender.ConsoleAuditSender;
 import com.waylend.synthetic_starter.monitoring.Audit.sender.KafkaAuditSender;
 import com.waylend.synthetic_starter.monitoring.metrics.MetricsAspect;
 
@@ -41,7 +43,12 @@ public class SyntheticAutoConfiguration {
     public AuditSender kafkaAuditSender(KafkaTemplate<String, String> kafkaTemplate) {
         return new KafkaAuditSender(kafkaTemplate);
     }
-
+    
+    @Bean
+    @Primary
+    public AuditSender auditSender(){
+        return new ConsoleAuditSender();
+    }
 
     @Bean
     public MetricsAspect metricsAspect(MeterRegistry registry) {
--- synthetic-starter/src/main/java/com/waylend/synthetic_starter/monitoring/Audit/sender/ConsoleAuditSender.java
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/monitoring/Audit/sender/ConsoleAuditSender.java
@@ -1,19 +1,11 @@
 package com.waylend.synthetic_starter.monitoring.Audit.sender;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-@Primary
-@ConditionalOnProperty(
-    name = "weyland.audit.mode", 
-    havingValue = "CONSOLE", 
-    matchIfMissing = true
-)
 public class ConsoleAuditSender implements AuditSender {
 
     @Override
