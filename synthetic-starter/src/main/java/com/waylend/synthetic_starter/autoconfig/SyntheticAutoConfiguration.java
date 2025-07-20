00f96662f911e4f9e3d13ed259953d64bc9dfc41
sharkioso <tetkin04@mail.ru>
2025-07-20 23:49:46 +0500

    доделано

---
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/autoconfig/SyntheticAutoConfiguration.java
@@ -0,0 +1,38 @@
+package com.waylend.synthetic_starter.autoconfig;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+import com.waylend.synthetic_starter.command.srevice.CommandService;
+import com.waylend.synthetic_starter.monitoring.Audit.AuditAspect;
+import com.waylend.synthetic_starter.monitoring.Audit.sender.AuditSender;
+import com.waylend.synthetic_starter.monitoring.metrics.MetricsAspect;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
+@AutoConfiguration
+public class SyntheticAutoConfiguration {
+
+    @Bean
+    public CommandService commandService() {
+        return new CommandService();
+    }
+
+    @Bean
+    public AuditAspect auditAspect(AuditSender sender) {
+        return new AuditAspect(sender);
+    }
+
+    @Bean
+    public MeterRegistry meterRegistry()
+    {
+        return new SimpleMeterRegistry();
+    }
+
+    @Bean
+    public MetricsAspect metricsAspect(MeterRegistry registry) {
+        return new MetricsAspect(registry);
+    }
+}
--- synthetic-starter/src/main/java/com/waylend/synthetic_starter/monitoring/Audit/sender/ConsoleAuditSender.java
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/monitoring/Audit/sender/ConsoleAuditSender.java
@@ -1,11 +1,13 @@
 package com.waylend.synthetic_starter.monitoring.Audit.sender;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@Primary
 public class ConsoleAuditSender implements AuditSender {
 
     @Override
