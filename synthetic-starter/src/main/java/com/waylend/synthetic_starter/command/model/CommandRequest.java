b2be7078faaad7f1db405306e593a45c4065efe9
sharkioso <tetkin04@mail.ru>
2025-07-20 13:29:40 +0500

    создал необходиме модели

---
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/command/model/CommandRequest.java
@@ -0,0 +1,29 @@
+package com.waylend.synthetic_starter.command.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+@Data
+public class CommandRequest {
+
+    @NotBlank
+    @Size(max = 1000)
+    private String description;
+    
+    private Prioryty prioryty;
+    
+    @NotBlank
+    @Size(max = 100)
+    private String author;
+
+    @Pattern
+    (
+        regexp = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:d{3}Z$",
+        message = "Время должно быть в формате ISO-8601"
+    )
+    private String time;
+
+}
---
+++ synthetic-starter/src/main/java/com/waylend/synthetic_starter/command/model/Prioryty.java
@@ -0,0 +1,6 @@
+package com.waylend.synthetic_starter.command.model;
+
+public enum Prioryty {
+    COMMON,
+    CRITICAL
+}
